6ff0823abfe6e07487d8378574bdc0030bd10cd4
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2026-04-06 23:43:13 +0700

    feat: update sah

--- lib/constants.lua
+++ lib/constants.lua
@@ -7,13 +7,13 @@ m.VERSIONS = {
     version = "v0.7.1",
     note = "latest",
     url = m.REPO_BASE_URL .. "v0.7.1.tar.gz",
-    sha256 = "95654fb64d0c053c2add2f86830228ef01e560dccf51345804cd044f8f697693",
+    sha256 = "0df7affff21cd87ed298e6a3970ed08a1dd66a6efa676454ee5b091ad503badf",
   },
   {
     version = "v0.7.0",
     note = "",
     url = m.REPO_BASE_URL .. "v0.7.0.tar.gz",
-    sha256 = "e84467764403a41816127c63e7e5ac808a7e01ba1d30e4a3c2279baa07ad46be",
+    sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
   },
 }
 
